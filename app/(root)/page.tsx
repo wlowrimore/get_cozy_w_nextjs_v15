f64e3d052eaabeaf41fe95ee46e7bd20f7dc5fd1
William Lowrimore <wlowrimore@gmail.com>
2024-10-28 22:35:02 -0500

    creates dynamic searchbar, adds utils date function, shadcn, startup card with fake data.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,27 @@
-export default function Home() {
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Nikki" },
+      _id: 1,
+      description: "This is a description.",
+      image:
+        "https://images.unsplash.com/photo-1516192518150-0d8fee5425e3?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -9,6 +32,20 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
