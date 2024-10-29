f4761802ff1b2ebf4432be18910b05d5dc3c84e1
William Lowrimore <wlowrimore@gmail.com>
2024-10-29 18:14:36 -0500

    updates sanity to include live updating of added startups.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -9,8 +9,7 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -31,12 +30,15 @@ export default async function Home({
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+            posts.map((post: any) => (
+              <StartupCard key={post?._id} post={post as StartupTypeCard} />
+            ))
           ) : (
             <p className="no-results">No Startups Found</p>
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
