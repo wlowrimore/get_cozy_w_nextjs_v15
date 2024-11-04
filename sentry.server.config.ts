5e244850018375706b36986abc8750e8e3d8b3f3
William Lowrimore <wlowrimore@gmail.com>
2024-11-03 19:12:33 -0600

    installs sentry and adds "report bug" sentry button.  IMPORTANT:  Turbo pack had to be disabled for this to take effect.

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1d9e275431f5723e1f2cda2796162061@o4508236937101312.ingest.us.sentry.io/4508236940509184",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
